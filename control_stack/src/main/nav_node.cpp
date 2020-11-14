74288c42d870a4d693bc03a40109bfb61bd1f4ff
zfish617 <zjf219@seas.upenn.edu>
2020-11-13 20:01:52 -0500

    obstacle cb, cmakelists

--- control_stack/src/main/nav_node.cpp
+++ control_stack/src/main/nav_node.cpp
@@ -3,11 +3,18 @@
 #include "cs/main/state_machine.h"
 #include "config_reader/config_reader.h"
 #include <string>
+#include <control_stack/Obstacles.h>
 
 
 namespace params {
 CONFIG_STRING(map, "pf.map");
 }
+
+
+void obstacle_callback(const control_stack::Obstacles::ConstPtr& msg) {
+  ROS_INFO("I heard: [%f]", msg->circles[0].center.x);
+}
+
 
 int main(int argc, char** argv) {
   // config file
@@ -31,6 +38,11 @@ int main(int argc, char** argv) {
                   &cs::main::StateMachine::UpdateLaser,
                   &state_machine);
   */
+
+  ros::Subscriber obstacle_sub =
+    n.subscribe("/obstacles",
+                1000,
+                obstacle_callback);
 
   // setup command publisher
   ros::Publisher command_pub = n.advertise<geometry_msgs::Twist>(
